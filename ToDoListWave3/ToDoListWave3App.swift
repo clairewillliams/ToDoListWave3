dab8d3145f296ca2ff29a126e5d76c48f770c816
Claire <clairewillliams@gmail.com>
2023-07-18 13:09:29 -0700

    Initial Commit

---
+++ ToDoListWave3/ToDoListWave3App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListWave3App.swift
+//  ToDoListWave3
+//
+//  Created by Claire Williams on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListWave3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
